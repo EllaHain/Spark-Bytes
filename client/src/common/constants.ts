3034874d0c4e446de4678d1e0b952795f422f864
mmyman <56144148+mmyman@users.noreply.github.com>
2024-04-24 13:18:32 -0400

    updated url to my url

--- client/src/common/constants.ts
+++ client/src/common/constants.ts
@@ -1,4 +1,4 @@
 export const API_URL: string =
   process.env.NODE_ENV === "production"
-    ? "http://backend-production-316c.up.railway.app"
+    ? "http://spark-bytes-project-team1-production-026e.up.railway.app"
     : "http://localhost:5005";
